a486326aaac59f8e6c3172ca0237657893ae736e
SendSafely <github@sendsafely.com>
2020-05-03 12:28:45 -0400

    Publish Synchronize Dropzone with Zendesk Agent List example
    Publish Synchronize Dropzone with Zendesk Agent List example

---
+++ Synchronize Dropzone with Zendesk Agent List/Javascript Example (Node)/README.md
@@ -0,0 +1,37 @@
+# Instructions for running SendSafely Dropzone Sync to Zendesk Script #
+
+This script will synchronize a list of Dropzone recipients in SendSafely with your list of agents in Zendesk. This is useful for cases where you want to make sure that all of your Zendesk agents have access to files that are uploaded through a Dropzone and have access revoked when they are removed from Zendesk. 
+
+In order to run the script, you will need to have Node.js installed. Here are instructions on how to install node and run the script:
+
+**1)** Visit https://nodejs.org/en/ and install the most recent version of Node.js.
+
+**2)** You will need to update a few variables at the top of the script. The following lines will need to be edited:
+
+```var ssHost = "https://company_name.sendsafely.com";
+var zdHost = "https://company_name.zendesk.com";
+var ssApiKey = "PUT_YOUR_SENDSAFELY_API_KEY_HERE";
+var ssApiSecret = "PUT_YOUR_SENDSAFELY_API_SECRET_HERE";
+var zdUsername = "PUT_YOUR_ZENDESK_USERNAME_HERE";
+var zdPassword = "PUT_YOUR_ZENDESK_PASSWORD_HERE";
+```
+
+**3)** Save the edited script and open a command line window. Navigate to the folder where you downloaded the script and run the following commands to install three required node modules (this command must be run from the same folder where the script is stored):
+
+`npm install @sendsafely/sendsafely`
+
+`npm install window`
+
+`npm install jquery`
+
+`npm install sjcl`
+
+**4)** Next, run the script using the following command:
+
+`node ZendeskSyncDropzone.js`
+
+The script will analyze the list of agents from Zendesk and will:
+- Add users to the Dropzone that are not already on the Dropzone recipients list
+- Remove users from the Dropzone list if they are not in the list of active agents
+
+The script does NOT add or remove any agents from Zendesk. 
---
+++ Synchronize Dropzone with Zendesk Agent List/Javascript Example (Node)/ZendeskSyncDropzone.js
@@ -0,0 +1,125 @@
+var SendSafely = require('@sendsafely/sendsafely');
+const Window = require('window');
+const window = new Window();
+const self = window;
+var $ = require("jquery")(window);
+const sjcl = require("sjcl");
+
+var ssHost = "https://company_name.sendsafely.com";
+var zdHost = "https://company_name.zendesk.com";
+var ssApiKey = "PUT_YOUR_SENDSAFELY_API_KEY_HERE";
+var ssApiSecret = "PUT_YOUR_SENDSAFELY_API_SECRET_HERE";
+var zdUsername = "PUT_YOUR_ZENDESK_USERNAME_HERE";
+var zdPassword = "PUT_YOUR_ZENDESK_PASSWORD_HERE";
+
+var sendSafely = new SendSafely(ssHost, ssApiKey, ssApiSecret);
+var zendeskMembers = [];
+var sendSafelyMembers = [];
+var sendSafelyMemberIds = [];
+var ssGroupId;
+
+sendSafely.on('sendsafely.error', function(error, errorMsg) {
+    console.log(error)
+});
+
+var zdRequest = $.ajax({
+    url: zdHost + "/api/v2/users.json?role=agent",
+    type: "GET",
+    timeout: 25000,
+    headers: {
+        'Authorization': "Basic " + Buffer.from(zdUsername + ":" + zdPassword).toString('base64')
+    },
+    crossDomain: true,
+    async: false,
+    retryCount: 2 //Need to Implement.
+});
+
+console.log(zdRequest.responseJSON);
+var matches = zdRequest.responseJSON.users;
+if (matches) {
+    for (var i = 0; i < matches.length; i++) {
+        zendeskMembers.push(matches[i].email.toLowerCase());
+        console.log("Got Zendesk User: " + matches[i].email.toLowerCase());
+    }
+
+}
+
+zdRequest = $.ajax({
+    url: zdHost + "/api/v2/users.json?role=admin",
+    type: "GET",
+    timeout: 25000,
+    headers: {
+        'Authorization': "Basic " + Buffer.from(zdUsername + ":" + zdPassword).toString('base64')
+    },
+    crossDomain: true,
+    async: false,
+    retryCount: 2 //Need to Implement.
+});
+console.log(zdRequest.responseJSON);
+var matches = zdRequest.responseJSON.users;
+for (var i = 0; i < matches.length; i++) {
+    zendeskMembers.push(matches[i].email.toLowerCase());
+    console.log("Got Zendesk User: " + matches[i].email.toLowerCase());
+}
+
+var groups = makeRequest("GET", "/api/v2.0/user/dropzone-recipients/");
+console.log(groups);
+var members = groups.responseJSON.recipientEmailAddresses;
+
+for (var j = 0; j < members.length; j++) {
+    sendSafelyMembers.push(members[j].toLowerCase());
+    console.log("Got SendSafely User: " + members[j].toLowerCase());
+}
+
+for (var i = 0; i < sendSafelyMembers.length; i++) {
+    if ($.inArray(sendSafelyMembers[i], zendeskMembers) == -1) {
+        console.log("REMOVE " + sendSafelyMembers[i]);
+        var result = makeRequest("DELETE", "/api/v2.0/user/dropzone-recipients/", JSON.stringify({
+            "userEmail": sendSafelyMembers[i]
+        }));
+        console.log(result);
+    }
+}
+
+for (var i = 0; i < zendeskMembers.length; i++) {
+    if ($.inArray(zendeskMembers[i], sendSafelyMembers) == -1) {
+        console.log("ADD " + zendeskMembers[i]);
+        var result = makeRequest("PUT", "/api/v2.0/user/dropzone-recipients/", JSON.stringify({
+            "userEmail": zendeskMembers[i]
+        }));
+        console.log(result);
+    }
+}
+
+function makeRequest(method, url, messageData) {
+
+    var timestamp = new Date().toISOString().substr(0, 19) + "+0000"; //2014-01-14T22:24:00+0000;
+
+    var messageString = ssApiKey + url + timestamp;
+    if (messageData != "" && messageData != null) {
+        messageString += messageData;
+    }
+    var signature = signMessage(messageString);
+
+    return $.ajax({
+        url: ssHost + url,
+        type: method,
+        timeout: 25000,
+        data: messageData == null ? null : messageData,
+        contentType: "application/json",
+        headers: {
+            'ss-api-key': ssApiKey,
+            'ss-request-timestamp': timestamp,
+            'ss-request-signature': signature,
+            'ss-request-api': 'JS_API'
+        },
+        crossDomain: true,
+        async: false,
+        retryCount: 2 //Need to Implement.
+    })
+}
+
+function signMessage(messageString) {
+    var hmacFunction = new sjcl.misc.hmac(sjcl.codec.utf8String.toBits(ssApiSecret), sjcl.hash.sha256); // Key, Hash
+    return sjcl.codec.hex.fromBits(hmacFunction.encrypt(messageString));
+}
